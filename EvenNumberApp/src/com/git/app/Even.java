b9c3599aaada2798c2c80034e21fa88293d15a2e
annakurian96 <annakurian96@gmail.com>
2020-02-26 12:18:24 +0530

    1st commit

---
+++ EvenNumberApp/src/com/git/app/Even.java
@@ -0,0 +1,15 @@
+package com.git.app;
+
+public class Even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Even Numbers");
+		for(int i=0;i<10;i++){
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
